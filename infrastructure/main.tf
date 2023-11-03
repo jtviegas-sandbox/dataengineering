253a606cd247033061f87b8fd128cf5996067d36
João Tiago Viegas <jtgv@novonordisk.com>
2023-11-03 13:26:32 +0100

    tweaks there

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,37 @@
 variable "region" {type = string}
 variable "solution" {type = string}
 variable "environment" {type = string}
-variable "data_bucket" {type = string}
+variable "datalake_bucket" {type = string}
 
 provider "aws" {
   region  = var.region
 }
 
 resource "aws_s3_bucket" "datalake" {
-  bucket = var.data_bucket
+  bucket = var.datalake_bucket
 
   tags = {
     solution    = var.solution
     environment = var.environment
   }
+}
+
+resource "aws_s3_bucket_object" "bronze" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "bronze/"
+    source = "/dev/null"
+}
+
+resource "aws_s3_bucket_object" "silver" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "silver/"
+    source = "/dev/null"
+}
+
+resource "aws_s3_bucket_object" "gold" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "gold/"
+    source = "/dev/null"
 }
 
 
